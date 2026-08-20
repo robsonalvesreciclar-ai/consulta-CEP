d8d78ad4b6892e2d1107ed7a3279e4715329a6ea
ROBSON ALVES DE LIMA <robson.alima2@senacsp.edu.br>
2026-08-20 19:38:47 -0300

    Att CEP

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
-// 1 captura dos elementos
+const campoTexto = document.getElementById("campoTexto");
+const btnConsultar = document.getElementById("btnConsultar");
+const btnLimpar = document.getElementById("btnLimpar");
+const display = document.getElementById("display");
 
-const campoTexto = document.getElementById('campoTexto');
-const btnConsultar = document.getElementById('btnConsultar');
-const btnLimpar = document.getElementById('btnLimpar');
-const display = document.getElementById('display');
+btnConsultar.addEventListener("click", function () {
 
+    let cep = campoTexto.value;
 
-btnConsultar.addEventListener("click", function() {
-    const campoTextoInput = campoTexto.value;
-    if (campoTextoInput === "") {
-        alert("ERRO !!!! Digite um CEP!!!");
+    if (cep == "") {
+        alert("Digite um CEP!");
         return;
     }
-    const url = `https://viacep.com.br/ws/${campoTextoInput}/json/`;
+
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+
     fetch(url)
-        .then(function(resposta) {
+        .then(function (resposta) {
             return resposta.json();
         })
-        .then(function(dados) {
-            console.log(dados);
+        .then(function (dados) {
+
             if (dados.erro) {
                 alert("CEP não encontrado!");
                 return;
             }
-            display.innerHTML = `CEP: ${dados.cep}<br>`;
-            display.innerHTML += `Logradouro: ${dados.logradouro}<br>`;
-            display.innerHTML += `Bairro: ${dados.bairro}<br>`;
-            display.innerHTML += `Cidade: ${dados.localidade}<br>`;
-            display.innerHTML += `Estado: ${dados.uf}<br>`;
+
+            display.innerHTML = "CEP: " + dados.cep + "<br>";
+            display.innerHTML += "Logradouro: " + dados.logradouro + "<br>";
+            display.innerHTML += "Bairro: " + dados.bairro + "<br>";
+            display.innerHTML += "Cidade: " + dados.localidade + "<br>";
+            display.innerHTML += "Estado: " + dados.uf;
+
             display.style.display = "block";
         });
 });
-btnLimpar.addEventListener("click", function() {
+btnLimpar.addEventListener("click", function () {
+
     campoTexto.value = "";
     display.innerHTML = "";
     display.style.display = "none";
-
 });
 
 
